43cc4e45fd3d5e1c0bc49b2c084f0b66c682c97b
cp-20 <n.naoki.1587@gmail.com>
2025-06-23 01:02:32 +0900

    🍞 🍥 🍤 update error handling

--- src/index.ts
+++ src/index.ts
@@ -10,29 +10,24 @@ type Environment = {
 const app = new Hono<Environment>();
 
 app.get("/icon/:username", async (c) => {
-	try {
-		const origin = c.env.TRAQ_ORIGIN;
-		const username = c.req.param("username");
-		const requestUrl = `${origin}/api/v3/public/icon/${username}`;
+	const origin = c.env.TRAQ_ORIGIN;
+	const username = c.req.param("username");
+	const requestUrl = `${origin}/api/v3/public/icon/${username}`;
 
-		const request = c.req.raw;
-		const imageOptions = getCfImageOptions(c.req.url);
-		const options: RequestInit<CfProperties> = {
-			cf: {
-				image: imageOptions,
-			},
-		};
+	const request = c.req.raw;
+	const imageOptions = getCfImageOptions(c.req.url);
+	const options: RequestInit<CfProperties> = {
+		cf: {
+			image: imageOptions,
+		},
+	};
 
-		const imageRequest = new Request(requestUrl, {
-			headers: request.headers,
-		});
+	const imageRequest = new Request(requestUrl, {
+		headers: request.headers,
+	});
 
-		const res = await fetch(imageRequest, options);
-		return res;
-	} catch (err) {
-		console.error("Error fetching icon:", err);
-		return c.text("Internal Server Error", 500);
-	}
+	const res = await fetch(imageRequest, options);
+	return res;
 });
 
 export default app;
