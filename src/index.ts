5f50c020c40d54ce058b413454f8e63e77f4503d
cp-20 <n.naoki.1587@gmail.com>
2025-06-23 00:57:41 +0900

    ☕ 🍥 🍭 fix res

--- src/index.ts
+++ src/index.ts
@@ -28,7 +28,7 @@ app.get("/icon/:username", async (c) => {
 		});
 
 		const res = await fetch(imageRequest, options);
-		return res.clone();
+		return res;
 	} catch (err) {
 		console.error("Error fetching icon:", err);
 		return c.text("Internal Server Error", 500);
