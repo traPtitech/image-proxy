859a1a8a9fd73a71ee757c799658bfeca0e7040c
cp-20 <n.naoki.1587@gmail.com>
2025-06-23 02:27:03 +0900

    🫑 🍣 🍅 色々消してスッキリさせた

--- src/index.ts
+++ src/index.ts
@@ -4,36 +4,6 @@ import { ResponseHeader } from "hono/utils/headers";
 
 const app = new Hono();
 
-// const CACHE_HIT_HEADER = "X-Image-Proxy-Cache";
-
-// const wrapResponseWithCacheHeader = (
-// 	response: Response,
-// 	cacheStatus: "HIT" | "MISS",
-// ): Response => {
-// 	const headers = new Headers(response.headers);
-// 	headers.set(CACHE_HIT_HEADER, cacheStatus);
-// 	return new Response(response.body, {
-// 		status: response.status,
-// 		statusText: response.statusText,
-// 		headers,
-// 	});
-// };
-
-// const fetchOrCache = async (
-// 	request: Request,
-// 	fetcher: (request: Request) => Promise<Response>,
-// ) => {
-// 	const cacheResponse = await caches.default.match(request);
-// 	if (cacheResponse) {
-// 		return wrapResponseWithCacheHeader(cacheResponse, "HIT");
-// 	}
-// 	const response = await fetcher(request);
-// 	if (200 <= response.status && response.status < 300) {
-// 		caches.default.put(request, response.clone());
-// 	}
-// 	return wrapResponseWithCacheHeader(response, "MISS");
-// };
-
 app.get("/icon/:username", async (c) => {
 	const username = c.req.param("username");
 	const requestUrl = `https://q.trap.jp/api/v3/public/icon/${username}`;
@@ -49,14 +19,12 @@ app.get("/icon/:username", async (c) => {
 	};
 
 	const res = await fetch(imageRequest, options);
-	// const res = await fetchOrCache(imageRequest, (req) => fetch(req, options));
 
 	const responseHeaders: Partial<
 		Record<ResponseHeader | "X-Image-Proxy-Cache", string>
 	> = {
 		"Cache-Control": "public, max-age=3600, s-maxage=3600",
 		"Content-Type": res.headers.get("Content-Type") ?? undefined,
-		// [CACHE_HIT_HEADER]: res.headers.get(CACHE_HIT_HEADER) ?? "MISS",
 	};
 
 	if (res.status === 304) {
