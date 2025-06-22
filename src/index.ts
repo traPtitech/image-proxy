e53bad4c181584e9a9d5ab87e936ff5d5b1a35c4
cp-20 <n.naoki.1587@gmail.com>
2025-06-23 01:08:56 +0900

    🌮 🥑 🌽 fix spelling error

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,7 @@ const app = new Hono();
 
 app.get("/icon/:username", async (c) => {
 	const username = c.req.param("username");
-	const requestUrl = `https://q.trpa.jp/api/v3/public/icon/${username}`;
+	const requestUrl = `https://q.trap.jp/api/v3/public/icon/${username}`;
 
 	const request = c.req.raw;
 	const imageOptions = getCfImageOptions(c.req.url);
