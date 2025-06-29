4b66b8dc2389d924f5fb6dc3016c8d8ef360673e
cp-20 <n.naoki.1587@gmail.com>
2025-06-29 21:32:27 +0900

    `/api/v3/public/icon/:username` にも対応するように

--- src/index.ts
+++ src/index.ts
@@ -4,32 +4,36 @@ import { ResponseHeader } from "hono/utils/headers";
 
 const app = new Hono();
 
-app.get("/icon/:username", async (c) => {
-	const username = c.req.param("username");
-	const requestUrl = `https://q.trap.jp/api/v3/public/icon/${username}`;
+app.on(
+	"GET",
+	["/icon/:username", "/api/v3/public/icon/:username"],
+	async (c) => {
+		const username = c.req.param("username");
+		const requestUrl = `https://q.trap.jp/api/v3/public/icon/${username}`;
 
-	const imageOptions = getCfImageOptions(c.req.url);
-	const requestHeaders = new Headers();
-	requestHeaders.set("User-Agent", "traP Image Proxy");
+		const imageOptions = getCfImageOptions(c.req.url);
+		const requestHeaders = new Headers();
+		requestHeaders.set("User-Agent", "traP Image Proxy");
 
-	const imageRequest = new Request(requestUrl, { headers: requestHeaders });
+		const imageRequest = new Request(requestUrl, { headers: requestHeaders });
 
-	const options: RequestInit<CfProperties> = {
-		cf: { image: imageOptions },
-	};
+		const options: RequestInit<CfProperties> = {
+			cf: { image: imageOptions },
+		};
 
-	const res = await fetch(imageRequest, options);
+		const res = await fetch(imageRequest, options);
 
-	const responseHeaders: Partial<Record<ResponseHeader, string>> = {
-		"Cache-Control": "public, max-age=3600, s-maxage=3600",
-		"Content-Type": res.headers.get("Content-Type") ?? undefined,
-	};
+		const responseHeaders: Partial<Record<ResponseHeader, string>> = {
+			"Cache-Control": "public, max-age=3600, s-maxage=3600",
+			"Content-Type": res.headers.get("Content-Type") ?? undefined,
+		};
 
-	if (res.status === 304) {
-		return c.body(null, 304, responseHeaders);
-	}
+		if (res.status === 304) {
+			return c.body(null, 304, responseHeaders);
+		}
 
-	return c.body(await res.arrayBuffer(), 200, responseHeaders);
-});
+		return c.body(await res.arrayBuffer(), 200, responseHeaders);
+	},
+);
 
 export default app;
